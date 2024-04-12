2294b3e90a0de5c978541d5483dd91a48ca96bdc
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2024-04-11 21:32:05 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"modelo A", imei:"1111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:"567890", modelo:"modelo X", imei:"222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
